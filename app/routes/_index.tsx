51fe63bbc7b57747ff86dedb5bc96bb45bcfab2b
Karl <karlcereno@gmail.com>
2024-05-22 17:29:12 -0400

    Make canvas size logic

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from '@remix-run/node';
+import { useState } from 'react';
 import Select from '~/components/ui/Select';
 import { StringMap } from '~/types/utilityTypes';
 
@@ -9,17 +10,27 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export type CanvasType =
+export type CanvasSize =
   | 'MOBILE'
   | 'TABLET'
   | 'LAPTOP'
   | 'DESKTOP'
   | 'ULTRAWIDE';
 
+export type ConfigForm = {
+  canvasSize: CanvasSize;
+};
+
 export default function Index() {
+  const INITIAL_CONFIG_FORM: ConfigForm = {
+    canvasSize: 'MOBILE',
+  };
+
+  const [configForm, setConfigForm] = useState<ConfigForm>(INITIAL_CONFIG_FORM);
+  console.log('Index ~ configForm:', configForm);
   // const [showMenu, setShowMenu] = React.useState(true);
 
-  const canvasSizeOptions: CanvasType[] = [
+  const canvasSizeOptions: CanvasSize[] = [
     'MOBILE',
     'TABLET',
     'LAPTOP',
@@ -27,8 +38,24 @@ export default function Index() {
     'ULTRAWIDE',
   ];
 
+  const getWidth = (canvasSize: CanvasSize) => {
+    const tailwindBreakpoints = {
+      MOBILE: 'max-w-[375px]', // Small screens and up
+      TABLET: 'max-w-[768px]', // Medium screens and up
+      LAPTOP: 'max-w-[1024px]', // Large screens and up
+      DESKTOP: 'max-w-[1440px]', // Extra large screens and up
+      ULTRAWIDE: 'max-w-[2560px]', // 2X large screens and up
+    };
+
+    return tailwindBreakpoints[canvasSize];
+  };
+
+  const width = getWidth(configForm.canvasSize);
+  console.log('Index ~ width:', width);
+
   const updateConfigForm = (updatedConfig: StringMap) => {
-    console.log('updateConfigForm ~ config:', updatedConfig);
+    const updatedConfigForm = { ...configForm, ...updatedConfig };
+    setConfigForm(updatedConfigForm);
   };
   return (
     <div className="">
@@ -43,7 +70,9 @@ export default function Index() {
         />
       </div>
 
-      <div className="Canvas"></div>
+      <div className="flex justify-center mx-10">
+        <div className={`${width} bg-red-300 w-full`}>Canvas</div>
+      </div>
     </div>
   );
 }
